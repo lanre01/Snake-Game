fbaf76e284f8db417d66056e6ddd21a06adb20b7
Lawal Alongbija <lawala249@gmail.com>
2023-12-13 19:54:26 +0000

    feat: added to the documentation in the code

--- code/SnakeGame/src/main/java/com/example/snakeGame/Paddle.java
+++ code/SnakeGame/src/main/java/com/example/snakeGame/Paddle.java
@@ -5,19 +5,19 @@ import javafx.scene.paint.Color;
 /**
  * paddle class creates a rectangle on the screen
  * the paddle must not be touched by the snake otherwise game ends.
- * the paddles may be able to move
- * needs a points x, y , graphics context, needs to be able to check if the snake has eaten it.
  */
 
 public class Paddle extends GameObject implements movable {
 
-
-    public static final Color BORDER_COLOR = Color.GREEN.darker().darker();
-    public static final Color INNER_COLOR = Color.GREEN;
+    public static final Color INNER_COLOR = Color.DARKRED;
 
     public static final int DEF_MOVE_AMOUNT = 5;
 
-
+    /**
+     * Paddle constructor
+     * @param xPosition the x position of the paddle on the view.
+     * @param yPosition the y position of the paddle on the view.
+     */
     public Paddle(int xPosition, int yPosition) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
@@ -34,7 +34,7 @@ public class Paddle extends GameObject implements movable {
 
     @Override
     public void draw(GraphicsContext graphics) {
-        graphics.setFill(Color.DARKRED);
+        graphics.setFill(INNER_COLOR);
         graphics.fillRect(xPosition, yPosition, this.objectWidth, this.objectHeight);
     }
 
